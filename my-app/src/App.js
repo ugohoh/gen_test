3b5dfcae7753e6b2509eea03788e4002a806cdfc
Hugo Bringout <hugo.bringout@epita.fr>
2022-12-05 17:46:27 +0100

    v4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+function addOne() {
+  window.alert("TODO : Call to backend to add one !!!");
+}
+function removeOne() {
+  window.alert("TODO : Call to backend to remove one !!!");
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Current count : <code>0</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div class="boutonss">
-          <button class="mybtonplus"> + 1 </button>
-          <button class="mybtonmoins"> - 1 </button>
+          <button class="mybtonplus" onClick={addOne}> + 1 </button>
+          <button class="mybtonmoins" onClick={removeOne}> - 1 </button>
         </div>
       </header>
     </div>
